5acd1fec86601b0718be99829c4c53351658384b
PrimeITArmenia <primellc.office@gmail.com>
2024-12-19 00:40:23 +0400

    fix db error

--- Backend/mongoose.js
+++ Backend/mongoose.js
@@ -1,13 +1,10 @@
 import mongoose from 'mongoose';
 
-const MONGODB_URI = process.env.MONGODB_URI
+const MONGODB_URI = process.env.MONGODB_URI;
 
 export const connectDB = async () => {
     try {
-        await mongoose.connect(MONGODB_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        await mongoose.connect(MONGODB_URI);
         console.log('Connected to MongoDB');
     } catch (error) {
         console.error('Failed to connect to MongoDB:', error);
